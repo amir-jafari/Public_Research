80ab070c62dfbb8eff9c43b8c3d725b43cb51b49
Twallett <tywallett@gmail.com>
2025-07-14 08:15:18 -0400

    no exclamation marks

--- Fairfax_County/data/data-downloader.py
+++ Fairfax_County/data/data-downloader.py
@@ -1,4 +1,4 @@
 #%%
-!wget -O 'FairfaxCounty.zip' 'https://gwu.box.com/shared/static/wvgmx7n7tbrhuk140t8tm9zkb2z5top9.zip'
-!unzip FairfaxCounty.zip 
-!rm FairfaxCounty.zip 
+wget -O 'FairfaxCounty.zip' 'https://gwu.box.com/shared/static/wvgmx7n7tbrhuk140t8tm9zkb2z5top9.zip'
+unzip FairfaxCounty.zip 
+rm FairfaxCounty.zip 
